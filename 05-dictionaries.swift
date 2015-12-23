bf124efb2c9a181c99eb5ba5ed3867b4ead1b02c
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-12-23 23:30:35 +0100

    feat: add 05-dictionnaries

---
+++ 05-dictionaries.swift
@@ -0,0 +1,17 @@
+//: # Swift basics - Dictionary
+
+//: ## Dictionary
+//: - Typed Dictionary
+
+var solarDistance  = [
+  "Mercury" : 57_910_000,
+  "Venus"   : 108_200_000,
+  "Earth"   : 149_600_000,
+  "Mars"    : 225_300_000,
+  "Jupiter" : 778_500_000,
+  "Saturn"  : 1_433_000_000,
+  "Uranus"  : 2_877_000_000,
+  "Neptune" : 4_498_000_000
+]
+
+solarDistance["Earth"]
